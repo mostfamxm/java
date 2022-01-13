61d5f13627f9177a536f51e41cc17cf1ed5284f2
mostfa mhameed <83960057+mostfamxm@users.noreply.github.com>
2022-01-13 15:15:29 +0200

    Add files via upload

---
+++ src/loopsAndConditions/Try.java
@@ -0,0 +1,16 @@
+package loopsAndConditions;
+
+public class Try {
+
+	public static void main(String[] args) {
+		 LoopsExercise l=new LoopsExercise();
+		System.out.println(l.fibonacci(5));
+		System.out.println(l.sumAllFromMinToMax(2, 6));
+		System.out.println(l.sumSevensFromMinToMax(1, 14));
+		System.out.println(l.nextMeeting(3, 5, 2021, 5));
+	}
+}
+	
+	
+
+
